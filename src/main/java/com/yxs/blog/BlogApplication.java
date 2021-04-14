a8ee8726cc4996486158e837664ee490569c3fc7
yanxinshi1 <492556935@qq.com>
2021-04-14 13:55:19 +0800

    配置部署

--- src/main/java/com/yxs/blog/BlogApplication.java
+++ src/main/java/com/yxs/blog/BlogApplication.java
@@ -1,9 +1,13 @@
 package com.yxs.blog;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@MapperScan("com.yxs.blog.myblog.*.dao")
 public class BlogApplication {
 
 	public static void main(String[] args) {
